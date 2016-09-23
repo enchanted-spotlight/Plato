32c438ac7f168eae076e7b7177582e72393adb3f
ClarabelleCheng-Yue <cachengyue@gmail.com>
2016-09-22 19:49:51 -0700

    Add node modules, package json, and test file for travis

--- Test.js
+++ Test.js
@@ -1,1 +1,37 @@
-console.log('Hello World');
+//chai documentation: http://chaijs.com/api/bdd/
+var chai = require('chai');
+var request = require('supertest');
+var express = require('express');
+var should = chai.should();
+var expect = chai.expect;
+
+//--------------- UNIT TEST ---------------// 
+// focuses on single method or class
+// tests one thing at a time
+// doesn't talk across network or to database
+describe('The test should pass', function() {
+  it('should pass the tests', function() {
+    expect(typeof 'string').to.equal('string');
+  });
+});
+
+/*
+//do this for other unit tests:
+describe('What is this test generally for?', function() {
+  it('write a mini test', function() {
+  //optional operations
+	expect(your code's result).to.equal(the desired result);
+  });
+});
+*/
+
+// ----------- INTEGRATION TEST ------------//
+// ensures diff parts of a system work correctly together
+// involves more than one part of a system
+// does code auth? talk to network? database? correctly?
+
+// ----------- END 2 END TEST -------------//
+// test if app works in real life situations
+
+//------------ Visual test? ------------- //
+// test if UI hasn't changed unexpectedly
