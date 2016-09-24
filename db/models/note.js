61dbd38378d85260e7af27608ada77a2eca52105
YOO629 <joeyu629@gmail.com>
2016-09-23 17:13:13 -0700

    Set up models for DB

--- db/models/note.js
+++ db/models/note.js
@@ -1,1 +1,11 @@
 const mongoose = require('mongoose');
+
+const noteSchema = mongoose.Schema({
+  user_id: String,
+  // not sure what notes should be
+  notes: Array,
+  // this should probably be unique for a user
+  title: String
+});
+
+module.exports = mongoose.model('Note', noteSchema);
