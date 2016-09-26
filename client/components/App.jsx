7ef675e1a1b98ead2fa79000847921ea1e38943e
Jon Garrett <jonathan.garrett@gmail.com>
2016-09-26 14:26:03 -0700

    Add props validators, source map for webpack, and install superagent

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -3,7 +3,6 @@ import request from 'superagent';
 
 import NoteList from './NoteList.jsx';
 import SearchBar from './SearchBar.jsx';
-<<<<<<< c7419caab52c614b25d9a5507bc8ef3308dde971
 
 class App extends React.Component {
   constructor(props) {
@@ -31,48 +30,5 @@ class App extends React.Component {
     );
   }
 }
-=======
-
-
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      articles: [
-        {
-          id: 1,
-          title: 'JavaScript Intro',
-          text: 'JavaScript is super fun.'
-        },
-        {
-          id: 2,
-          title: 'JavaScript for Beginners',
-          text: 'Functions on objects are methods.'
-        },
-        {
-          id: 3,
-          title: 'JavaScript Frameworks',
-          text: 'Angular sure is nice, but this React stuff seems cool too.'
-        },
-      ]
-    }
-  }
-
-  handleTermChange(term) {
-    console.log('Search term: ' + term);
-  }
-
-  render() {
-    return (
-      <div>
-        <h1>Howdy Dan!</h1>
-        <SearchBar onTermChange={this.handleTermChange} />
-        <NoteList notes={this.state.articles} />
-      </div>
-    );
-  }
-}
-
->>>>>>> Change browse componenet to NoteList and create NoteItem component
 
 export default App;
