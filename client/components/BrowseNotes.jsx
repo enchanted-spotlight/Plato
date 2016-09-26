d3aa505b78ef9f9d4f9f08d8cd950e030a838dfe
Jon Garrett <jonathan.garrett@gmail.com>
2016-09-26 12:23:44 -0700

    Create SearchBar component and include it in BrowseNotes

--- client/components/BrowseNotes.jsx
+++ client/components/BrowseNotes.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
 
+import SearchBar from './SearchBar.jsx';
+
 const BrowseNotes = props => (
   <div className="notes-list">
     <h2>Your Notes</h2>
+    <SearchBar />
   </div>
 );
 
