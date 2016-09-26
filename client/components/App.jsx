e19ca28653a210523aa4e7e190c0f0a83897d9d1
Jon Garrett <jonathan.garrett@gmail.com>
2016-09-26 16:27:05 -0700

    Implement successful GET request with superagent and refactor state

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,34 +1,20 @@
 import React from 'react';
+import request from 'superagent';
 
 import NoteList from './NoteList.jsx';
 import SearchBar from './SearchBar.jsx';
-
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      articles: [
-        {
-          id: 1,
-          user_id: 'Jon',
-          title: 'JavaScript Intro',
-          text: 'JavaScript is super fun.'
-        },
-        {
-          id: 2,
-          user_id: 'Jon',
-          title: 'JavaScript for Beginners',
-          text: 'Functions on objects are methods.'
-        },
-        {
-          id: 3,
-          user_id: 'Jon',
-          title: 'JavaScript Frameworks',
-          text: 'Angular sure is nice, but this React stuff seems cool too.'
-        },
-      ]
+      user: 'Jon',
+      articles: []
     };
+    const urlUser = `api/${this.state.user}`;
+    request.get(urlUser, (err, res) => {
+      this.setState({ articles: JSON.parse(res.text) });
+    });
     this.statics = {
       handleTermChange: term => console.log('Search term: ', term)
     };
