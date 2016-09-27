000b1756ec6b4f31e5222f010d0bbac09cd08c55
Jon Garrett <jonathan.garrett@gmail.com>
2016-09-27 13:43:54 -0700

    Small style changes to components and create addtl. enzyme tests

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -12,29 +12,28 @@ class App extends React.Component {
       username: '',
       articles: []
     };
-
-    this.statics = {
-      handleTermChange: term => console.log('Search term: ', term),
-      fetchNotes: (username) => {
-        this.setState({ username });
-        const urlUser = `api/${username}`;
-        request.get(urlUser, (err, res) => {
-          this.setState({ articles: JSON.parse(res.text) });
+    this.searchNotes = term => console.log('Search term: ', term);
+    this.fetchNotes = (username) => {
+      const urlUser = `api/${username}`;
+      return request('GET', urlUser)
+        .then((res) => {
+          this.setState({ articles: JSON.parse(res.text) })
+        }, (err) => {
+          console.log('Error fetching user notes: ', err);
         });
-      }
     };
   }
 
   render() {
     return (
-      <div>
-        <LogIn fetchNotes={this.statics.fetchNotes} />
+      <div className="plato-app">
+        <LogIn fetchNotes={this.fetchNotes} />
         <h1>{this.state.username ?
           `Howdy ${this.state.username}!`
           : 'Howdy!'
           }
         </h1>
-        <SearchBar onTermChange={this.statics.handleTermChange} />
+        <SearchBar onTermChange={this.searchNotes} />
         <NoteList notes={this.state.articles} />
       </div>
     );
