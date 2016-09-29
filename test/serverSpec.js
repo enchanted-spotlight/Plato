b2b9c02072591d3596b18cb99e1b27e323d9e1a9
YOO629 <joeyu629@gmail.com>
2016-09-28 17:11:34 -0700

    Travis

--- test/serverSpec.js
+++ test/serverSpec.js
@@ -14,9 +14,6 @@ const Note = require('../db/models/note');
 
 describe('Plato', () => {
   before((done) => {
-    const dummyUser = new User({
-      name: 'dummyUser'
-    });
     done();
   });
 
@@ -46,12 +43,7 @@ describe('Plato', () => {
   // focuses on single method or class
   // tests one thing at a time
   // doesn't talk across network or to database
-  describe('The test should pass', () => {
-    it('should pass the tests', () => {
-      expect(typeof 'string').to.equal('string');
-    });
-  });
-
+  
   /*
   //do this for other unit tests:
   describe('What is this test generally for?', function() {
