0ddb0c6757aa2ecaf3a3341c86424c7c2e167679
Jon Garrett <jonathan.garrett@gmail.com>
2016-09-29 18:25:52 -0700

    (practice) Refactor login and db query to use redux
    Using Redux Thunk middleware. Not yet rendering DB results.
    
    Have array of results being fetched and passed to function.
    
    TODO: render notes

--- client/index.jsx
+++ client/index.jsx
@@ -1,99 +1,110 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore, combineReducers } from 'redux';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
 import connect from 'react-redux';
+import thunkMiddleware from 'redux-thunk';
+import createLogger from 'redux-logger';
 
-const todo = (state, action) => {
-  switch (action.type) {
-  case 'ADD_TODO': {
-    return {
-      id: action.id,
-      text: action.text,
-      completed: false
-    };
-  }
-  case 'TOGGLE_TODO': {
-    if (state.id !== action.id) {
-      return state;
-    }
-    return {
-      ...state,
-      completed: !state.completed
-    };
-  }
-  default: {
-    return state;
-  }
-  }
+const USER_LOGIN = 'USER_LOGIN';
+const REQUEST_NOTES = 'REQUEST_NOTES';
+const RECEIVE_NOTES = 'RECEIVE_NOTES';
+
+const loggerMiddleware = createLogger();
+
+const requestNotes = (username) => {
+  return {
+    type: REQUEST_NOTES,
+    username
+  };
 };
 
-const todos = (state = [], action) => {
-  switch (action.type) {
-  case 'ADD_TODO': {
-    return [
-      ...state,
-      todo(undefined, action)
-    ];
-  }
-  case 'TOGGLE_TODO': {
-    return state.map(t => todo(t, action));
-  }
-  default: {
-    return state;
-  }
-  }
+// TODO: Add error handling to receiveNotes
+const receiveNotes = (username, results) => {
+  console.log('results given to receiveNotes: ', results);
+  return {
+    type: RECEIVE_NOTES,
+    username,
+    notes: results.map(child => child.data),
+    recievedAt: Date.now()
+  };
 };
 
-const visibilityFilter = (state = 'SHOW_ALL', action) => {
-  switch (action.type) {
-  case 'SET_VISIBILITY_FILTER': {
-    return action.filter;
-  }
-  default: {
-    return state;
-  }
-  }
+const fetchNotes = (username) => {
+  return (dispatch) => {
+    dispatch(requestNotes(username));
+    return fetch(`api/${username}`)
+      .then(response => response.json())
+      .then(json => dispatch(receiveNotes(username, json)));
+  };
 };
 
-const todoApp = combineReducers({ todos, visibilityFilter });
-const store = createStore(todoApp);
+const userLogin = (state = '', action) => {
+  if (action.type === USER_LOGIN) {
+    return action.username;
+  }
+  return state;
+};
 
-let nextTodoId = 0;
-class TodoApp extends React.Component {
+const platoApp = combineReducers({ userLogin });
+const store = createStore(
+  platoApp,
+  window.devToolsExtension && window.devToolsExtension(),
+  applyMiddleware(
+    thunkMiddleware,
+    loggerMiddleware
+  )
+);
+
+class Login extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { username: '' };
+  }
   render() {
     return (
-      <div>
-        <button
-          onClick={() => {
-            store.dispatch({
-              type: 'ADD_TODO',
-              text: 'Test',
-              id: nextTodoId += 1
-            });
-          }}
-        >
-        Add Todo
-        </button>
-        <ul>
-          {this.props.todos.map(todo =>
-            <li key={todo.id}>
-              {todo.text}
-            </li>
-          )}
-        </ul>
-      </div>
+      <form
+        className="login"
+        onSubmit={(e) => {
+          e.preventDefault();
+          // Dispath this.state.username so that store is updated
+          store.dispatch({
+            type: USER_LOGIN,
+            username: this.state.username
+          });
+          this.setState({ username: '' });
+        }}
+      >
+        <h3>Login:</h3>
+        <input
+          type="text"
+          value={this.state.username}
+          onChange={
+            e => this.setState({ username: e.target.value })
+          }
+        />
+        <input
+          type="submit"
+        />
+      </form>
     );
   }
 }
 
+const PlatoApp = () =>  (
+  <div>
+    <h1>This is Plato Note Taker!</h1>
+    <Login />
+  </div>
+);
+
 const render = () => {
   ReactDOM.render(
-    <TodoApp
-      todos={store.getState().todos}
-    />,
-    document.getElementById('root')
+    <PlatoApp />,
+    document.getElementById('app')
   );
 };
 
+
 store.subscribe(render);
 render();
+store.dispatch(fetchNotes('Jon')).then(() => console.log(store.getState()));
