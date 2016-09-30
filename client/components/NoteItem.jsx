33ecaabe3aaa7f4cad4d1fd9ae7765efad5518d4
Daniel Snyder <daniel.c.snyder@gmail.com>
2016-09-30 15:04:19 -0700

    (cleanup) Remove console.log

--- client/components/NoteItem.jsx
+++ client/components/NoteItem.jsx
@@ -2,8 +2,7 @@ import React from 'react';
 
 const NoteItem = (props) => {
   const displayNote = () => {
-    console.log('Clicked! Trying to display saved note...');
-    console.log(props.text, props.title);
+    // console.log('Clicked! Trying to display saved note...');
     props.loadNote(props.text, props.title);
   };
 
