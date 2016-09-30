9d33b3760129ba3ebfc0bd45d9dfdc3b36ce68dd
Jon Garrett <jonathan.garrett@gmail.com>
2016-09-30 16:36:21 -0700

    Complete single file Redux implementation of basic features

--- client/index.jsx
+++ client/index.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-<<<<<<< cd44f6c8950214440c103a69513dd189bbdc8b4c
 import { createStore, applyMiddleware } from 'redux';
 import thunkMiddleware from 'redux-thunk';
 import createLogger from 'redux-logger';
@@ -10,14 +9,6 @@ import platoApp from './plato';
 
 const loggerMiddleware = createLogger();
 
-
-const store = createStore(
-  platoApp.reduce.default,
-
-const dispatcher = (action, value) => store.dispatch(action(value));
-
-const PlatoComp = platoApp.components.default;
-
 const render = () => {
   ReactDOM.render(
     <PlatoApp />,
@@ -25,7 +16,5 @@ const render = () => {
   );
 };
 
-
 store.subscribe(render);
 render();
-store.dispatch(fetchNotes('Jon')).then(() => console.log(store.getState()));
