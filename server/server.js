3a722b92c609e0ca42d063b49c27df24dc34be27
Jon Garrett <jonathan.garrett@gmail.com>
2016-10-06 16:53:16 -0700

    Set up inital connection to slack with @slack/client module

--- server/server.js
+++ server/server.js
@@ -25,3 +25,31 @@ app.listen(3000, () => {
 });
 
 module.exports = app;
+
+// Slack API integration:
+
+const slack = require('@slack/client');
+const RtmClient = slack.RtmClient;
+const CLIENT_EVENTS = slack.CLIENT_EVENTS;
+const RTM_EVENTS = slack.RTM_EVENTS;
+
+const token = process.env.SLACK_API_TOKEN || '';
+
+const rtmOptions = {
+  // logLevel: 'debug'
+};
+
+const rtm = new RtmClient(token, rtmOptions);
+rtm.start();
+
+rtm.on(CLIENT_EVENTS.RTM.AUTHENTICATED, (rtmStartData) => {
+  console.log(`Logged in as ${rtmStartData.self.name} of team ${rtmStartData.team.name}, but not yet connected to a channel.`);
+});
+
+rtm.on(RTM_EVENTS.MESSAGE, (message) => {
+  console.log(`A message was captured: `, message);
+  rtm.sendMessage('rtm.sendMessage function', 'C2KE7FVV3', (err, msg) => {
+    msg.text = "Updated!";
+  });
+});
+
