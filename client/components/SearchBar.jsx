d141bfd1d509ca1c2d3925e7c4686a1df677c461
Daniel Snyder <daniel.c.snyder@gmail.com>
2016-10-06 11:01:29 -0700

    (feat) Begin structuring search component

--- client/components/SearchBar.jsx
+++ client/components/SearchBar.jsx
@@ -1,15 +1,41 @@
 import React from 'react';
+import request from 'superagent';
 
 class SearchBar extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { term: '' };
+    this.state = {
+      username: ''
+    };
+  }
+
+  componentWillReceiveProps() {
+    this.setState({
+      username: this.props.userName
+    });
   }
 
   onInputChange(term) {
-    // this.setState({ term });
-    this.props.onTermChange(term);
+    const urlUser = `api/${this.state.username}`;
+    console.log('This is firing');
+    request('POST', urlUser)
+    // this is going to have to change, res.text is no longer just a string
+      .then((res) => {
+        this.setState({ articles: JSON.parse(res.text) });
+      }, (err) => {
+        console.log('Error fetching user notes: ', err);
+        // if we have a 404, that means that the user doesn't have any notes
+        // in which case we dont need to display a note list
+        // to display an empty note list, we set articles = []
+        if (err.status === 404) {
+          this.setState({ articles: [] });
+        }
+      });
   }
+
+  // onInputChange(term) {
+  //   this.props.onTermChange(term);
+  // }
 
   render() {
     return (
@@ -27,7 +53,7 @@ class SearchBar extends React.Component {
 }
 
 SearchBar.propTypes = {
-  onTermChange: React.PropTypes.func
+  userName: React.PropTypes.string
 };
 
 export default SearchBar;
