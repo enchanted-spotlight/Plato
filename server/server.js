4d8ed99fe15077bb7264113cc142f8070796e2e7
Jon Garrett <jonathan.garrett@gmail.com>
2016-10-07 11:41:45 -0700

    Implement initial test connection for socket.io

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
 
+
 // Require db so it sets up connection
 require('./db');
 const express = require('express');
@@ -11,6 +12,23 @@ const noteRouter = require('./config/routes');
 const app = express();
 middleware(app, express);
 
+// Socket.io setup
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+
+io.on('connection', (socket) => {
+  console.log('A user connected via socket.io!');
+  const testMessage = {
+    type: 'message',
+    channel: 'C2KE7FVV3',
+    user: 'U2KEC1PV5',
+    text: 'capture this message for data structure',
+    ts: '1475865416.000003',
+    team: 'T2KE19RLG'
+  };
+  socket.emit('incoming slack message', testMessage);
+});
+
 // routing
 app.use('/api', noteRouter);
 
@@ -19,7 +37,7 @@ app.get('/', (req, res) => {
 });
 
 // initialize server
-app.listen(3000, () => {
+http.listen(3000, () => {
   console.log('Plato is listening on port 3000 ...');
 });
 
