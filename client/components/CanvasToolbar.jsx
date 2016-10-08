1703ee53aae76c569052c967cc2823df7b5de97e
YOO629 <joeyu629@gmail.com>
2016-10-08 16:45:31 -0700

    Start Canvas

---
+++ client/components/CanvasToolbar.jsx
@@ -0,0 +1,57 @@
+import React from 'react';
+import { Button } from 'react-materialize';
+
+class CanvasToolbar extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      width: '',
+      height: ''
+    };
+
+    this.updateWidth = (e) => {
+      this.setState({ width: e.target.value });
+    };
+
+    this.updateHeight = (e) => {
+      this.setState({ height: e.target.value });
+    };
+  }
+
+  // eslint-disable-next-line
+  render() {
+    return (
+      <div>
+        <span className="saveCanvas">
+          <Button floating className="red" onClick={this.props.saveCanvas} icon="save" />
+        </span>
+        <span className="strokeStyle">
+          <Button floating className="red" onClick={() => this.props.updateStyle("red")} />
+          <Button floating className="blue" onClick={() => this.props.updateStyle("blue")} />
+          <Button floating className="green" onClick={() => this.props.updateStyle("green")} />
+          <Button floating className="black" onClick={() => this.props.updateStyle("black")} />
+        </span>
+        <span className="lineWidth">
+          <Button floating className="red" icon="add" onClick={() => this.props.incrementLineWidth()} />
+          <Button floating className="red" icon="remove" onClick={() => this.props.decrementLineWidth()} />
+        </span>
+        <span className="canvasHeight">
+          <Button floating className="green" icon="swap_vert" onClick={() => this.props.incrementCanvasHeight()} />
+          <Button floating className="green" icon="swap_horiz" onClick={() => this.props.incrementCanvasWidth()} />
+        </span>
+      </div>
+    );
+  }
+}
+
+CanvasToolbar.propTypes = {
+  saveCanvas: React.PropTypes.func,
+  updateStyle: React.PropTypes.func,
+  incrementLineWidth: React.PropTypes.func,
+  decrementLineWidth: React.PropTypes.func,
+  incrementCanvasHeight: React.PropTypes.func,
+  incrementCanvasWidth: React.PropTypes.func
+};
+
+export default CanvasToolbar;
