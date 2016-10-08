3b5a051ecac109d8a8386e8db850bc18625fe3eb
Jon Garrett <jonathan.garrett@gmail.com>
2016-10-07 19:25:34 -0700

    Begin final refactor to redux

---
+++ client/components/ChatClient.jsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import { sendChatMessage } from './../actions';
+
+import ChatMessagesDisplay from './ChatMessagesDisplay.jsx';
+
+class ChatClient extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      store: props.store,
+      message: '',
+      user: props.username
+    };
+    console.log('ChatClient constructor state value: ', this.state.store.getState());
+  }
+  render() {
+    return (
+      <div className="chat-client-container">
+        <ChatMessagesDisplay />
+        <form
+          onSubmit={(e) => {
+            e.preventDefault();
+            console.log('Message input on chat client: ', this.state.message);
+            // Form is not properly re rendering after setState
+            // Redux conflict? This is a problem with all app's forms
+            this.setState({ message: '' });
+            console.log('store values? ', this.store.getState());
+            const submitObj = {
+              user: this.state.user,
+              message: this.state.message
+            };
+            console.log('submit object: ', submitObj);
+            this.state.store.dispatch(sendChatMessage(submitObj));
+          }}
+        >
+          <input
+            type="text"
+            onChange={e => this.setState({ message: e.target.value })}
+          />
+        </form>
+      </div>
+    );
+  }
+}
+
+ChatClient.propTypes = {
+  store: React.PropType.object,
+  username: React.PropType.object
+};
+
+export default ChatClient;
---
+++ client/index.jsx
@@ -0,0 +1,106 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import thunkMiddleware from 'redux-thunk';
+import createLogger from 'redux-logger';
+import { Row, Col, Navbar, NavItem } from 'react-materialize';
+import io from 'socket.io-client';
+
+import LogIn from './components/LogIn.jsx';
+import NoteList from './components/NoteList.jsx';
+import SearchBar from './components/SearchBar.jsx';
+import SpeechToTextEditor from './components/SpeechToTextEditor.jsx';
+import MediumDraft from './components/MediumDraft.jsx';
+import reducers from './reducer.js';
+import ChatClient from './components/ChatClient.jsx';
+
+const socket = io();
+socket.on('incoming slack message', (data) => {
+  console.log('client side socket received data: ', data);
+});
+socket.on('slack message archive', (data) => {
+  // console.log('message history received: ', data);
+});
+
+const loggerMiddleware = createLogger();
+
+const store = createStore(
+  reducers,
+  window.devToolsExtension && window.devToolsExtension(),
+  applyMiddleware(
+    thunkMiddleware,
+    loggerMiddleware
+  )
+);
+
+const dispatcher = (action, value) => store.dispatch(action(value));
+
+const App = (props) => {
+  const {
+    store,
+    username,
+    savedNotes,
+    textEditor,
+    speechEditor,
+    sessionTitle
+  } = props;
+  return (
+    <div className="plato-app">
+      <Navbar brand="Plato" right>
+        <NavItem href="">Login</NavItem>
+        <NavItem href="">Signout</NavItem>
+      </Navbar>
+      <LogIn
+        dispatcher={dispatcher}
+      />
+      <SearchBar />
+      <Row>
+        <Col s={2} className="blue-grey lighten-3 base-col-height">
+          <div className="blue-grey lighten-3 column-header-lists">
+            <h3>All Notes</h3>
+          </div>
+          <NoteList
+            store={store}
+            username={username}
+            notes={savedNotes.notes}
+          />
+        </Col>
+
+        <Col s={2} className="grey lighten-2 base-col-height">
+          <SpeechToTextEditor />
+        </Col>
+
+        <Col s={6} className="base-col-height">
+          <MediumDraft
+            store={store}
+            username={username}
+            currentNote={textEditor}
+            currentNoteTitle={sessionTitle}
+          />
+        </Col>
+
+        <Col s={2} className="base-col-height">
+          <ChatClient
+            store={store}
+            username={username}
+          />
+        </Col>
+      </Row>
+    </div>
+  );
+};
+
+const render = () => {
+  ReactDOM.render(
+    <App
+      store={store}
+      {...store.getState()}
+    />,
+    document.getElementById('app')
+  );
+};
+
+
+store.subscribe(render);
+render();
