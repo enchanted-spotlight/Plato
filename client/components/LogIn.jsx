6d90dae767bfd1e2b87187946a9c465164b59e34
Jon Garrett <jonathan.garrett@gmail.com>
2016-10-08 16:11:33 -0700

    Add to oauth packages and fix bad merge

--- client/components/LogIn.jsx
+++ client/components/LogIn.jsx
@@ -35,11 +35,9 @@ class LogIn extends React.Component {
         <div className="twitterLogin">
           <a href="api/auth/login/twitter"> Sign in with Twitter </a>
         </div>
-
         <div className="slackLogin">
           <a href="api/auth/login/slack"> Sign in with Slack </a>
         </div>
-
         <div className="googleLogin">
           <a href="api/auth/login/google"> Sign in with Google </a>
         </div>
