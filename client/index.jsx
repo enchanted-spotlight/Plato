fb86cd76e25302f3c14acb388ed04a156b479aef
ClarabelleCheng-Yue <cachengyue@gmail.com>
2016-10-10 16:29:13 -0700

    Fix conflicts

--- client/index.jsx
+++ client/index.jsx
@@ -37,6 +37,55 @@ socket.on('incoming chat message', (data) => {
 socket.on('chat room archive', (data) => {
   store.dispatch(a.loadArchivedChatMessages(data));
 });
+// =======
+// const App = (props) => {
+//   const {
+//     // eslint-disable-next-line no-shadow
+//     store,
+//     username,
+//     savedNotes,
+//     textEditor,
+//     speechEditor,
+//     sessionTitle
+//   } = props;
+//   return (
+//     <div className="plato-app">
+//       <Navbar brand="Plato" right>
+//         <NavItem href="">Login</NavItem>
+//         <NavItem href="">Signout</NavItem>
+//       </Navbar>
+//       <LogIn
+//         dispatcher={dispatcher}
+//       />
+//       <SearchBar
+//         store={store}
+//         username={username}
+//       />
+//       <Row>
+//         <Col s={2} className="blue-grey lighten-3 base-col-height">
+//           <div className="blue-grey lighten-3 column-header-lists">
+//             <h3>All Notes</h3>
+//           </div>
+//           <NoteList
+//             store={store}
+//             username={username}
+//             notes={savedNotes.notes}
+//           />
+//         </Col>
+//         <Col s={10} className="session-container">
+//           <Session
+//             username={username}
+//             transcript={speechEditor}
+//             currentNote={textEditor}
+//             currentNoteTitle={sessionTitle}
+//             dispatcher={dispatcher}
+//           />
+//         </Col>
+//       </Row>
+//     </div>
+//   );
+// };
+// >>>>>>> Pass down more props from index
 
 
 const App = () => (
