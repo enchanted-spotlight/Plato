f8b1a237516e8cd72fa812989c3499674d9238bb
ClarabelleCheng-Yue <cachengyue@gmail.com>
2016-10-09 17:40:32 -0700

    Fix conflicts

--- client/components/Session.jsx
+++ client/components/Session.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
-import { Row, Col } from 'react-materialize';
+import { Row, Col, Button } from 'react-materialize';
+import { convertToRaw } from 'draft-js';
+import request from 'superagent';
+import { createEditorState } from 'medium-draft';
 
 import SpeechToTextEditor from './SpeechToTextEditor.jsx';
-import MyEditor from './MyEditor.jsx';
+import MediumEditor from './MediumDraft.jsx';
 
 class Session extends React.Component {
   constructor(props) {
@@ -19,31 +22,103 @@ class Session extends React.Component {
     this.state = {
       time: 0,
       currentNoteTitle: this.props.currentNoteTitle,
-      transcript: '',
+      transcript: createEditorState(), // '',
       currentNote: this.props.currentNote,
-      username: this.props.username
+    };
+
+    // this method should mirror the MyEditor component
+    this.onChange = (editorState) => {
+      console.log(editorState, 'speech to text state');
+      this.setState({ currentNote: editorState });
+      console.log(editorState, 'speech to text changed');
+    };
+
+    this.titleChange = (e) => {
+      this.setState({ currentNoteTitle: e.target.value });
+    };
+
+    // this method should mirror the MyEditor component
+    this.submitNote = () => {
+      // this will let us save the current content as rich text
+      const userNote = convertToRaw(this.state.currentNote.getCurrentContent());
+      const plainTextContent = this.state.currentNote.getCurrentContent().getPlainText();
+      const userTitle = this.state.title;
+      const username = this.props.username;
+      const url = 'api/save-note';
+
+      const toBeSent = {
+        user_id: username,
+        text: JSON.stringify(userNote),
+        plainText: JSON.stringify(plainTextContent),
+        title: userTitle
+      };
+
+      console.log(toBeSent, 'to be sent!!!!');
+
+      // submit the note to the server for storage in db
+      // request
+      //   .post(url)
+      //   .send({
+      //     user_id: username,
+      //     text: JSON.stringify(userNote),
+      //     plainText: JSON.stringify(plainTextContent),
+      //     title: userTitle
+      //   })
+      //   .set('Accept', 'application/json')
+      //   .end((err) => {
+      //     if (err) {
+      //       console.log('There is an error in submitNote: ', err);
+      //     } else {
+      //       this.props.fetchNotes(this.props.username);
+      //     }
+      //   });
     };
   }
 
+  componentWillReceiveProps(newProps) {
+    this.setState({
+      currentNote: newProps.currentNote,
+      currentNoteTitle: newProps.currentNoteTitle
+    });
+  }
+
   render() {
-    console.log(this.props, 'props');
+    console.log(this.props, 'session props');
+    console.log(this.state, 'session state');
     return (
-      <Row>
-        <Col s={2} className="grey lighten-2 base-col-height">
-          <SpeechToTextEditor
-            username={this.state.username}
-            fetchNotes={this.props.fetchNotes}
+      <div>
+        <Row>
+          <input
+            type="text"
+            value={this.state.title}
+            onChange={this.titleChange}
+            placeholder="Title"
           />
-        </Col>
-        <Col s={8} className="base-col-height">
-          <MyEditor
-            username={this.state.username}
-            currentNote={this.state.currentNote}
-            currentNoteTitle={this.state.currentNoteTitle}
-            fetchNotes={this.props.fetchNotes}
-          />
-        </Col>
-      </Row>
+          <Col s={2} className="grey lighten-2 base-col-height">
+            <SpeechToTextEditor
+              transcript={this.state.transcript}
+              fetchNotes={this.props.fetchNotes}
+              titleChange={this.titleChange}
+              onTranscriptChange={this.onChange}
+            />
+          </Col>
+
+          <Col s={8} className="base-col-height">
+            <MediumEditor
+              currentNote={this.state.currentNote}
+              currentNoteTitle={this.state.currentNoteTitle}
+              fetchNotes={this.props.fetchNotes}
+              titleChange={this.titleChange}
+              onNoteChange={this.onChange}
+            />
+          </Col>
+        </Row>
+        <Button
+          onClick={() => this.submitNote()}
+          waves="light"
+        >Submit
+        </Button>
+      </div>
     );
   }
 }
