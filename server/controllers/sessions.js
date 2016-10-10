b6849b9fd8a2c4de84fe9150782d66749527f505
ClarabelleCheng-Yue <cachengyue@gmail.com>
2016-10-10 01:05:57 -0700

    Add controller for session routing

---
+++ server/controllers/sessions.js
@@ -0,0 +1,62 @@
+const Session = require('./../models/session');
+
+const sessionController = {
+  saveSession(req, res) {
+    console.log(req.body);
+    const update = {
+      user_id: req.body.user_id,
+      title: req.body.title,
+      notesText: req.body.notes.text,
+      notesPlainText: req.body.notes.plainText,
+      transcriptText: req.body.transcript.text,
+      transcriptPlainText: req.body.transcript.plainText
+      // text: req.body.text,
+      // // where is the title going to be?
+      // plainTextContent: req.body.plainText
+    };
+    Session.findOneAndUpdate({
+      user_id: req.body.user_id,
+      title: req.body.title
+    }, update, { upsert: true }, (err, data) => {
+      if (err) {
+        res.status(500).end();
+      } else {
+        res.status(200).send(data);
+      }
+    });
+  },
+  retrieveAllUserSessions(req, res) {
+    Session.find({ user_id: req.params.user }, (err, data) => {
+      if (err) {
+        res.status(500).end();
+      } else if (data.length === 0) {
+        res.status(404).end();
+      } else {
+        res.status(200).send(data);
+      }
+    });
+  },
+  retrieveCertainUserSessions(req, res) {
+    const userInput = req.body.searchInput;
+    console.log(req.body.searchInput);
+    Session.find({ user_id: req.params.user, $text: { $search: userInput } }, (err, data) => {
+      if (err) {
+        res.status(500).end();
+      } else {
+        console.log(data);
+        res.status(200).send(data);
+      }
+    });
+  },
+  deleteUserSession(req, res) {
+    Session.findOneAndRemove({ _id: req.params.id }, (err, doc, result) => {
+      if (err) {
+        res.status(500).end();
+      } else {
+        res.status(200).end();
+      }
+    });
+  }
+};
+
+module.exports = sessionController;
