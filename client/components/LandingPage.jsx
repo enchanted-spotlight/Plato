2a04b21fdcb2761c0cdb7bb44d8398f49b62bd0e
Daniel Snyder <daniel.c.snyder@gmail.com>
2016-10-13 16:24:09 -0700

    (feat) Add columns for landing page

--- client/components/LandingPage.jsx
+++ client/components/LandingPage.jsx
@@ -123,6 +123,12 @@ const LandingPage = () => (
             </div>
           </div>
         </div>
+        <div className="footer-copyright">
+          <div className="container">
+            © 2014 Copyright Text
+            <a className="grey-text text-lighten-4 right" href="#!">More Links</a>
+          </div>
+        </div>
       </footer>
     </div>
   </div>
