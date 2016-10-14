05adc705f86f044d871f1e04638a8cc3d35911ef
YOO629 <joeyu629@gmail.com>
2016-10-14 11:01:08 -0700

    test

--- client/components/LandingPage.jsx
+++ client/components/LandingPage.jsx
@@ -1,19 +1,48 @@
 import React from 'react';
 import { Row, Col } from 'react-materialize';
 
-
 const loginButtonStyle = {
   height: '32px',
   width: '32px',
   border: '0',
   background: 'transparent'
 };
-
 const LandingPage = () => (
   <div>
     <div className="grey lighten-4">
       <div>
+        <div className="topContainer">
+          <div>
+            <h1 className="title center-align">Plato</h1>
+            <h3 className="center-align">The new note-taking experience</h3>
+          </div>
+        </div>
+        <div className="outerContainer">
+          <div className="container innerContainer">
+            <div className="row innerContainer">
+              <div className="col s4 innerContainer valign-wrapper center-align">
+                <div className="container">
+                  <i className="medium material-icons">settings_remote</i>
+                  <p> Tired of typing your notes? Let our speech to text editor do the heavy lifting for you. </p>
+                </div>
+              </div>
+              <div className="col s4 innerContainer valign-wrapper center-align">
+                <div className="container">
+                  <i className="medium material-icons">picture_in_picture</i>
+                  <p> Can't put your thoughts into words? Use our infinite canvas to draw them instead. </p>
+                </div>
+              </div>
+              <div className="col s4 innerContainer valign-wrapper center-align">
+                <div className="container">
+                  <i className="medium material-icons">message</i>
+                  <p>Need clarification? Use the class chat feature to share insights and problems.</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
         <div className="row container">
+          <h2 className="center-align"> Meet Us: </h2>
           <div className="col s3">
             <div className="card white">
               <img role="presentation" className="responsive-img" src="/../styles/images/cbelle.jpg" />
@@ -64,7 +93,7 @@ const LandingPage = () => (
             <div className="card white">
               <img role="presentation" className="responsive-img" src="/../styles/images/joejoe.jpg" />
               <div className="card-content">
-                <p className="center-align names">Joe Yu</p>
+                <p className="center-align names">Joe-Joe Yu</p>
                 <p className="orange-text medium italic center-align jobdesc">Software Engineer</p>
               </div>
               <div className="card-action">
@@ -111,23 +140,16 @@ const LandingPage = () => (
       <footer className="page-footer light-blue darken-2">
         <div className="container">
           <div className="row">
-            <div className="col l6 s12">
-              <h5 className="white-text">Plato</h5>
-              <p className="grey-text text-lighten-4">What do we want to put here???</p>
-            </div>
-            <div className="col l4 offset-l2 s12">
-              <h5 className="white-text">Links</h5>
-              <ul>
-                <li><a className="grey-text text-lighten-3" href="#!">a</a></li>
-              </ul>
-            </div>
+            <div className="col l6 s12" />
+          </div>
+        </div>
+        <div className="footer-copyright">
+          <div className="container">
+            © 2016 MIT License
           </div>
         </div>
       </footer>
     </div>
   </div>
-
-
 );
-
 export default LandingPage;
